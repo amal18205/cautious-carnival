2aedf858ffa908ead6654d2d313221ba10f4c8a0
amal18205 <blinkulili@gmail.com>
2026-07-22 11:25:05 +0100

    Connexion PostgreSQL et amélioration interface

--- app.py
+++ app.py
@@ -1,23 +1,90 @@
 from flask import Flask, render_template, request, redirect
+import psycopg2
+import time
 
 app = Flask(__name__)
 
-tasks = []
+def get_db():
+    return psycopg2.connect(
+        host="database",
+        database="tasksdb",
+        user="devops",
+        password="password"
+    )
+
+
+def init_db():
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 @app.route("/")
 def home():
+
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("SELECT id, name FROM tasks")
+    tasks = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
     return render_template("index.html", tasks=tasks)
+
 
 @app.route("/add", methods=["POST"])
 def add():
+
     task = request.form["task"]
-    tasks.append(task)
+
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO tasks(name) VALUES(%s)",
+        (task,)
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
     return redirect("/")
+
 
 @app.route("/delete/<int:id>")
 def delete(id):
-    tasks.pop(id)
+
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute(
+        "DELETE FROM tasks WHERE id=%s",
+        (id,)
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
     return redirect("/")
 
+
 if __name__ == "__main__":
+    time.sleep(10)
+    init_db()
     app.run(host="0.0.0.0", port=5000)
